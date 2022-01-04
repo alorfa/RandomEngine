e59dd3066425679e89aee4213f8bcc2e2fe9b6d9
alorfa <TheMrFmame@gmail.com>
2022-01-04 21:33:12 +0500

    add hitbox drawer

--- Game/Object/SpriteObject.cpp
+++ Game/Object/SpriteObject.cpp
@@ -1,32 +1,42 @@
 #include "SpriteObject.hpp"
-#include <SFML/Graphics/RenderTarget.hpp>
-#include "SpriteObject.hpp"
 
-#include "Game/Player.hpp"
+#include <SFML/Graphics/RenderTarget.hpp>
+
 #include <RandomEngine/API/Auxiliary/DEBUG.hpp>
 #include <RandomEngine/API/Auxiliary/print_vectors.hpp>
-
+#include <RandomEngine/API/Graphics/Shape.hpp>
 #include <RandomEngine/API/Math/RepulsionResult.hpp>
 #include <RandomEngine/API/Math/CollisionFunctions.hpp>
+#include "Game/Player.hpp"
+#include "Game/Settings.hpp"
 
 namespace game
 {
 	void SpriteObject::draw(sf::RenderTarget& target, sf::RenderStates states) const
 	{
-		sprite.setPosition(getPosition());
-		sprite.setScale(getScale());
-		sprite.setRotation(getRotation());
+		states.transform *= getTransform();
 
-		target.draw(sprite);
+		target.draw(sprite, states);
+
+		if (Settings::show_hitboxes)
+		{
+			Shape::createRectangle(hitbox, hitbox_vertices);
+			target.draw(hitbox_vertices, 5, sf::LinesStrip, states);
+		}
 	}
+	SpriteObject::SpriteObject()
+		: hitbox({ -0.5f, -0.5f }, { 0.5f, 0.5f }) 
+	{
+		for (auto& vert : hitbox_vertices)
+			vert.color = { 0, 0, 255 };
+	}
+
 	bool SpriteObject::touches(const Player& p) const
 	{
 		Rect player(p.getPosition() - p.getScale() * 0.5f);
 		player.max = player.min + p.getScale();
-		Rect obj(getPosition() - getScale() * 0.5f);
-		obj.max = obj.min + getScale();
 
-		return Collision::areIntersected(player, obj);
+		return Collision::areIntersected(player, getCollisionRect());
 	}
 	RepulsionResult SpriteObject::getRepulsionVector(const Player& p) const
 	{
@@ -43,10 +53,17 @@ namespace game
 	const PhysicalRect& SpriteObject::getPhysicalRect() const
 	{
 		physical_rect.direction = direction;
-		physical_rect.min = getPosition() - getScale() * 0.5f;
-		physical_rect.max = physical_rect.min + getScale();
 		physical_rect.center = getPosition();
+		physical_rect.min = hitbox.min * getScale() + getPosition();
+		physical_rect.max = hitbox.max * getScale() + getPosition();
 		physical_rect.movement = getPosition() - getPrevPos();
 		return physical_rect;
+	}
+	Rect SpriteObject::getCollisionRect() const
+	{
+		return Rect{
+			hitbox.min * getScale() + getPosition(),
+			hitbox.max * getScale() + getPosition()
+		};
 	}
 }
--- Game/Object/SpriteObject.hpp
+++ Game/Object/SpriteObject.hpp
@@ -12,9 +12,13 @@ namespace game
 	{
 		mutable Sprite sprite;
 		mutable PhysicalRect physical_rect;
+		mutable sf::Vertex hitbox_vertices[5];
 
 		void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 	public:
+		Rect hitbox;
+		SpriteObject();
+
 		bool touches(const Player&) const override;
 		RepulsionResult getRepulsionVector(const Player&) const override;
 
@@ -22,5 +26,6 @@ namespace game
 		void setArea(const vec2& p1, const vec2& p2);
 
 		const PhysicalRect& getPhysicalRect() const;
+		Rect getCollisionRect() const;
 	};
 }
